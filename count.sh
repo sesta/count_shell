932f00cafcf07a98a9ff3f783f709787a56bec4d
Sesta <sesta.ec5@gmail.com>
2015-11-01 13:11:45 +0900

    表示の仕方を変えました

--- count.sh
+++ count.sh
@@ -1,9 +1,37 @@
 #!/bin/sh
 
-SPACE='      '
+SPACE='                 '
 START_TIME=`date +%s`
 now_time=$START_TIME
 seconds=0
+
+function echoTime() {
+  s=$1
+  m=$(( ( s / 60 ) % 60 ))
+  h=$(( s / 60 / 60 ))
+  s=$(( s % 60 ))
+
+  if [ $h -lt 10 ] ; then
+    h_space='0'
+  else
+    h_space=''
+  fi
+
+  if [ $m -lt 10 ] ; then
+    m_space='0'
+  else
+    m_space=''
+  fi
+
+  if [ $s -lt 10 ] ; then
+    s_space='0'
+  else
+    s_space=''
+  fi
+
+  echo "    $SPACE\r\c"
+  echo "    $h_space$h:$m_space$m:$s_space$s\r\c"
+}
 
 while getopts as:m:h:t: OPT ; do
   case $OPT in
@@ -36,8 +64,7 @@ done
 
 if [ $seconds -ne 0 ] ; then
   while [ $seconds -gt $(( now_time - START_TIME )) ] ; do
-    echo " $SPACE\r\c"
-    echo " $(( seconds + START_TIME - now_time ))\r\c"
+    echoTime $(( seconds + START_TIME - now_time ))
 
     sleep 1s
 
@@ -48,12 +75,10 @@ if [ $seconds -ne 0 ] ; then
   echo "$alert\r\c"
 else
   while true ; do
-    echo " $SPACE\r\c"
-    echo " $(( now_time - START_TIME ))\r\c"
+    echoTime $(( now_time - START_TIME ))
 
     sleep 1s
 
     now_time=`date +%s`
   done
 fi
-
