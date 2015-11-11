fdbcc0a57ed8b8337e1ec69a81f48805d96e1d6c
Sesta <sesta.ec5@gmail.com>
2015-11-11 17:15:52 +0900

    数字をaaにしました

--- count.sh
+++ count.sh
@@ -4,14 +4,28 @@ START_TIME=`date +%s`
 now_time=$START_TIME
 seconds=0
 
+line_1=("888888" "    88" "888888" "888888" "88  88" "888888" "888888" "888888" "888888" "888888")
+line_2=("88  88" "    88" "    88" "    88" "88  88" "88    " "88    " "88  88" "88  88" "88  88")
+line_3=("88  88" "    88" "888888" "888888" "888888" "888888" "888888" "88  88" "888888" "888888")
+line_4=("88  88" "    88" "88    " "    88" "    88" "    88" "88  88" "    88" "88  88" "    88")
+line_5=("888888" "    88" "888888" "888888" "    88" "888888" "888888" "    88" "888888" "888888")
+
+
 function echoTime(){
   cols=`tput cols`
-  space=`makeSpace $(( ( cols - 9 )  / 2 ))`
+  space=`makeSpace $(( ( cols - 6*6 - 2*3 - 6*2 )  / 2 ))`
 
   s=$1
   m=$(( ( s / 60 ) % 60 ))
   h=$(( s / 60 / 60 ))
   s=$(( s % 60 ))
+
+  h_10=$(( h / 10 ))
+  h_1=$((  h % 10 ))
+  m_10=$(( m / 10 ))
+  m_1=$((  m % 10 ))
+  s_10=$(( s / 10 ))
+  s_1=$((  s % 10 ))
 
   if [ $h -lt 10 ] ; then
     h_space='0'
@@ -31,7 +45,13 @@ function echoTime(){
     s_space=''
   fi
 
-  echo "\n$space$h_space$h:$m_space$m:$s_space$s$space\n"
+  echo ""
+  echo "$space${line_1[h_10]}  ${line_1[h_1]}      ${line_1[m_10]}  ${line_1[m_1]}      ${line_1[s_10]}  ${line_1[s_1]}"
+  echo "$space${line_2[h_10]}  ${line_2[h_1]}  88  ${line_2[m_10]}  ${line_2[m_1]}  88  ${line_2[s_10]}  ${line_2[s_1]}"
+  echo "$space${line_3[h_10]}  ${line_3[h_1]}      ${line_3[m_10]}  ${line_3[m_1]}      ${line_3[s_10]}  ${line_3[s_1]}"
+  echo "$space${line_4[h_10]}  ${line_4[h_1]}  88  ${line_4[m_10]}  ${line_4[m_1]}  88  ${line_4[s_10]}  ${line_4[s_1]}"
+  echo "$space${line_5[h_10]}  ${line_5[h_1]}      ${line_5[m_10]}  ${line_5[m_1]}      ${line_5[s_10]}  ${line_5[s_1]}"
+  echo ""
 }
 
 function makeSpace(){
@@ -77,9 +97,9 @@ while getopts as:m:h:t: OPT ; do
 done
 
 if [ $seconds -ne 0 ] ; then
-  echo "\n\n"
+  echo "\n\n\n\n\n\n"
   while [ $seconds -gt $(( now_time - START_TIME )) ] ; do
-    cursorUp 3
+    cursorUp 7
     echoTime $(( seconds + START_TIME - now_time ))
 
     sleep 1s
@@ -87,12 +107,12 @@ if [ $seconds -ne 0 ] ; then
     now_time=`date +%s`
   done
 
-  cursorUp 3
+  cursorUp 7
   echo "\n$space Finish! $space\n$alert"
 else
-  echo "\n\n"
+  echo "\n\n\n\n\n\n"
   while true ; do
-    cursorUp 3
+    cursorUp 7
     echoTime $(( now_time - START_TIME ))
 
     sleep 1s
